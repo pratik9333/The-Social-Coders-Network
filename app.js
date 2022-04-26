9300d4ae50012a87e4ab4bfd9303ea694954e4a3
Pratik Aswani <pratikaswani@Pratiks-MacBook-Air.local>
2022-04-26 13:59:24 +0530

    FEATURE: added codechef platform

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const fileUpload = require("express-fileupload");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
+
 //cookies and file middleware
 app.use(cookieParser());
 
@@ -19,24 +21,10 @@ app.use(
   })
 );
 
-//morgan middleware
-//app.use(morgan("tiny"));
-
 //import all routes here
 const user = require("./api/user.api");
-const platform = require("./api/platform.api");
-
-// Access-Control-Allow-Origin: https://foo.example
-// Access-Control-Allow-Methods: POST, GET, OPTIONS
-// Access-Control-Allow-Headers: X-PINGOTHER, Content-Type
-// Access-Control-Max-Age: 86400
-
-app.use((req, res, next) => {
-  next();
-});
 
 //router middleware
 app.use("/api/v1", user);
-app.use("/api/v1", platform);
 
 module.exports = app;
